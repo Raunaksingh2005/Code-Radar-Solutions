d234fbdb54199f41048838728e7a6e896944835b
Raunaksingh2005 <singh.raunak2005@gmail.com>
2025-01-28 13:37:36 +0530

    Solution to Check LSB of a Number

--- Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 int main() {
     int a;
+    // Read the input value
     scanf("%d", &a);
+    
+    // Check the least significant bit
     if (a & 1) {
-        printf("1\n");
+        printf("1\n");  // LSB is 1 (odd number)
     } else {
-        printf("0\n");
+        printf("0\n");  // LSB is 0 (even number)
     }
+    
     return 0;
 }
