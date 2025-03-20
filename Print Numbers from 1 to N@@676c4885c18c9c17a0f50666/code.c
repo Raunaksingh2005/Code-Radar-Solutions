f51026ad7079e2844c47eb8f60f780bbf87575a3
Raunaksingh2005 <singh.raunak2005@gmail.com>
2025-03-20 20:50:57 +0530

    Solution to Print Numbers from 1 to N

--- Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
+++ Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
@@ -2,12 +2,8 @@
 int main() {
     int a;
     scanf("%d", &a);
-    int n=1;
-    for (int i = 1; i <= a; i++) {
-        for (int j = 1; j <=i; j++) {
-            printf("%d ",n);
-            n++;
-        }
+    for (int i = 1; i <= a; i++){
+        printf("%d ",n);
     }
     return 0;
 }
